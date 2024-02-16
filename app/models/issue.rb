42ef3f1c482c6d7994ee299f6b3509e381bd5a88
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-02-16 15:31:01 -0700

    Add ebay search and csv creation to issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class Issue < ApplicationRecord
   has_many :issue_features
   has_many :features, through: :issue_features
@@ -8,4 +9,29 @@ class Issue < ApplicationRecord
     ordered: 1,
     have: 2
   }
+
+  def incomplete?
+    title == nil
+  end
+
+  def missing
+    where(status: ["do_not_have", nil])
+  end
+
+  def ebay_search
+    formatted_title = title.parameterize(separator: '+')
+    "https://www.ebay.com/sch/i.html?_from=R40&_nkw=#{formatted_title}&_sacat=0"
+  end
+
+  def self.to_csv
+    attributes = %w{id title}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      Issue.all.each do |issue|
+        csv << attributes.map{ |attr| issue.send(attr) }
+      end
+    end
+  end
 end

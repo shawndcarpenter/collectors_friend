4a0ad55a2640624808de9dce0f5220ae9b51a85a
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-02-16 10:49:10 -0700

    Add enum to issue for status

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,10 @@ class Issue < ApplicationRecord
   has_many :issue_features
   has_many :features, through: :issue_features
   belongs_to :collection
+
+  enum status: {
+    do_not_have: 0,
+    ordered: 1,
+    have: 2
+  }
 end
